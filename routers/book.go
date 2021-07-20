5b9129287df41b258940064fb16b575f352a1e0e
Madaspe <danil.treshchev@yandex.ru>
2021-07-20 14:44:19 +0200

    add simple get book router

--- routers/book.go
+++ routers/book.go
@@ -1,16 +1,24 @@
 package routers
 
 import (
-	f "fmt"
+	"encoding/json"
+	"github.com/Madaspe/simple-messanger-go-api/models"
 	"github.com/go-noodle/adapt/gorilla"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	vars := gorilla.GetVars(r)
-	_, err := f.Fprintf(w, "%s", vars["id"])
+	id, _ := strconv.Atoi(vars["id"])
+
+	book := models.Book{Id: int64(id)}
+	err := json.NewEncoder(w).Encode(book)
 
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 }
